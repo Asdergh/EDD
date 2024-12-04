6f0db878ab0da87bf2cec7bf8780e67b763c8206
Asdergh <ramzanhac2005@gmail.com>
2024-12-05 01:56:06 +0300

    SeConv model was added

---
+++ src/SeConv/SeConvNet.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.layers import (
+    Input,
+    Conv2D,
+    Activation,
+    Flatten,
+    Dense,
+    BatchNormalization,
+    GlobalAveragePooling2D
+)
+from tensorflow.keras import (
+    Model,
+    Sequential
+)
+from modules import (
+    Conv,
+    Se
+)
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.metrics import (
+    Mean, Accuracy
+)
+from tensorflow import (
+    GradientTape,
+    function,
+    argmax
+)
+
+
+class SeConvNet(Model):
+
+    def __init__(
+            self, 
+            input_sh: tuple,
+            out_classes: int,
+            **kwargs
+    ) -> None:
+    
+        super().__init__(**kwargs)
+        self.input_sh = input_sh
+        self.out_cll = out_classes
+        self.model_ = self.build_model_()
+    
+    def build_model_(self):
+
+        input = Input(shape=self.input_sh)
+        conv = Conv(out_channels=128)(input)
+        conv = Se(in_channels=128, name="conv1")(conv)
+        conv = Conv(out_channels=64)(conv)
+        conv = Se(in_channels=64, name="conv2")(conv)
+        conv = Conv(out_channels=32)(conv)
+        conv = Se(in_channels=32, name="conv3")(conv)
+
+        ffn = Flatten()(conv)
+        ffn = Dense(units=128, activation="relu")(ffn)
+        ffn = Dense(units=64, activation="relu")(ffn)
+        ffn = Dense(units=self.out_cll, activation="sigmoid")(ffn)
+
+        return Model(inputs=input, outputs=ffn)
+    
+    def compile(self, optimizer, loss):
+
+        super().compile()
+        self.opt = optimizer
+        self.loss_fn = loss
+        
+        self.loss_tracker = Mean(name="loss tracker")
+        self.acc_tracker = Accuracy(name="acc tracker")
+    
+    @property
+    def metrics(self):
+        return [
+            self.loss_tracker,
+            self.acc_tracker
+        ]
+
+    def train_step(self, inputs):
+
+        images, labels = inputs
+        with GradientTape() as gr_tape:
+
+            pred_labels = self.model_(images)
+            loss = self.loss_fn(labels, pred_labels)
+        
+        grads = gr_tape.gradient(loss, self.model_.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.model_.trainable_variables))
+        
+        for metric in self.metrics:
+
+            if "loss" in metric.name:
+                metric.update_state(loss)
+            
+            else:
+                metric.update_state(labels, argmax(pred_labels, axis=-1))
+        
+        return {
+            metric.name: metric.result()
+            for metric in self.metrics
+        }
+    
+    @function
+    def call(self, inputs):
+        return self.total_model_(inputs)
+
+    
---
+++ src/SeConv/data_utils.py
@@ -0,0 +1,78 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import json as js
+import os
+import torch as th
+import cv2
+
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+from torchvision.transforms.v2 import (
+    Resize,
+    Normalize,
+    Compose
+)
+
+
+
+class ImageDetectionSet(Dataset):
+
+    def __init__(
+            self,
+            data_dir: str,
+            data_split: str,
+            transform: th.nn.Module = None
+    ) -> None:
+        
+
+        super().__init__()
+        self.tranform = transform
+        self.data_path = os.path.join(data_dir, data_split)
+        self.change_annotations_coco_()
+
+        annots_file = os.path.join(self.data_path, "_annotations.json")
+        with open(annots_file, "r") as js_file:
+            annots = js.load(js_file)
+        
+        self.images_js = annots["images"]
+        self.annots_js = annots["annotations"]
+    
+
+    def change_annotations_coco_(self):
+
+        annots_path = os.path.join(self.data_path, "_annotations.coco.json")
+        with open(annots_path, "r") as js_file:
+
+            data = js.load(js_file)
+            images = {sample["id"]: sample for sample in data["images"]}
+            annots = {sample["image_id"]: sample for sample in data["annotations"]}
+
+        data["images"] = images
+        data["annotations"] = annots
+        annots_path = os.path.join(self.data_path, "_annotations.json")
+
+        with open(annots_path, "w") as js_file:
+            js.dump(data, js_file)
+
+    def __len__(self):
+        return len(self.images_js)
+
+    def __getitem__(self, idx):
+
+        image = read_image(os.path.join(self.data_path, self.images_js[f"{idx}"]["file_name"])).to(th.float32)
+        image /= 255.0
+        if not self.tranform is None:
+            image = self.tranform(image)
+
+        image_label = self.annots_js[f"{idx}"]["category_id"]
+        image_bbox = np.array(self.annots_js[f"{idx}"]["bbox"])
+
+        return (
+            image.T,
+            image_label,
+            image_bbox
+        ) 
+
+
+
+        
